7d2cc96b90420d38544e56360ec7003739ec0c7c
Alexuc Eduard <alexuc.edy27@gmail.com>
2023-05-05 22:04:40 +0300

    First commit

---
+++ script.js
@@ -0,0 +1,16 @@
+// api url: http://www.omdbapi.com/?s=thor&apikey=72eac67a
+
+const movieSearchBox = document.getElementById('search-box');
+const resultGrid = document.getElementById('result-grid'); 
+
+async function getMovies(searchItem) {
+  const URL = `http://www.omdbapi.com/?s=${searchItem}&apikey=72eac67a`;
+  const response = await fetch(`${URL}`);
+  const data = await response.json();
+  // console.log(data);
+  if(data.Response == "True") {
+    displayMovieList(data.Search);
+  }
+}
+
+getMovies('inception');
