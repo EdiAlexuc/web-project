d957ba1565373ebc2b4c35c3d5b1ffe845ed9f18
Alexuc Eduard <alexuc.edy27@gmail.com>
2023-05-08 11:31:40 +0300

    Added display list

--- script.js
+++ script.js
@@ -15,6 +15,29 @@ async function getMovies(searchItem) {
 
 function displayMovieList(movies) {
   resultGrid.innerHTML = "";
+  movies.forEach(movie => {
+    const movieDiv = document.createElement("div");
+    movieDiv.classList.add("movie");
+    const moviePoster = document.createElement("img");
+    moviePoster.src = movie.Poster;
+    const movieTitle = document.createElement("h3");
+    movieTitle.innerText = movie.Title;
+    const movieYear = document.createElement("p");
+    movieYear.innerText = movie.Year;
+    const movieType = document.createElement("p");
+    movieType.innerText = movieType;
+    movieDiv.appendChild(moviePoster);
+    movieDiv.appendChild(movieTitle);
+    movieDiv.appendChild(movieYear);
+    movieDiv.appendChild(movieType);
+    movieDiv.appendChild(movieDiv); 
+  });
 }
+
+movieSearchBox.addEventListener("submit", (event) => {
+  event.preventDefault();
+  const searchItem = movieSearchBox.nextElementSibling.searchItem.value;
+  getMovies(searchItem);
+});
 
 getMovies('inception');
